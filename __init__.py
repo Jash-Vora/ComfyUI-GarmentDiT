ee6f87c07779e0377d59666c7dc16158fce1b52c
Jash-Vora <133149276+Jash-Vora@users.noreply.github.com>
2025-01-01 16:44:23 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
 from .GarmentDiT import GarmentEnhancementNode
+NODE_CLASS_MAPPINGS = { "GarmentEnhancementNode": GarmentEnhancementNode }
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "GarmentEnhancementNode": "Garment Enhancement"
+}
+__all__ = ["NODE_CLASS_MAPPINGS","NODE_DISPLAY_NAME_MAPPINGS]
