94766315b2e89058745e4d73a1d76158c20719a1
Jash-Vora <133149276+Jash-Vora@users.noreply.github.com>
2025-01-01 16:53:30 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ NODE_CLASS_MAPPINGS = { "GarmentEnhancementNode": GarmentEnhancementNode }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "GarmentEnhancementNode": "Garment Enhancement"
 }
-__all__ = ["NODE_CLASS_MAPPINGS","NODE_DISPLAY_NAME_MAPPINGS]
+__all__ = ["NODE_CLASS_MAPPINGS","NODE_DISPLAY_NAME_MAPPINGS"]
